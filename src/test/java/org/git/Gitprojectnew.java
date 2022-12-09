ca0ad1a8d79b3c69622387c8e6178767280e5693
thilagam <thilagam.selvan@gmail.com>
2022-12-09 15:06:47 +0530

    two methods

---
+++ src/test/java/org/git/Gitprojectnew.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class Gitprojectnew {
+
+	public void m1() {
+		System.out.println("Welcome to Manualtesting");
+	}
+	
+	public void m2() {
+		System.out.println("This is Gitproject");
+	}
+}
